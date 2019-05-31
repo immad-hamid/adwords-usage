9ee6c001d9d660ed65dd0664b1028252c490adf4
Immad Hamid <immadhamid@Immads-MacBook-Pro.local>
2019-05-31 05:57:21 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+
+// controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profiles = require('./controllers/profiles');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//       host : '127.0.0.1',
+//       user : 'postgres',
+//       password : '',
+//       database : 'smart-brain'
+//     }
+// });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// root
+app.get('/', (req, res) => { res.json('Success, but nothing at root') })
+
+
+// // register
+// app.post('/register', register.handleRegister(db, bcrypt))
+// // signin
+// app.post('/signin', signin.handleSignin(db, bcrypt))
+// // get users
+// app.get('/profiles', profiles.handleProfiles(db))
+// // get user
+// app.get('/profile/:id', profile.handleProfile(db))
+// // post image entries
+// app.put('/image', image.handleImage(db))
+
+// serving our applicaiton
+app.listen(3000, () => {
+    console.log('server is working at port 3000');
+});
